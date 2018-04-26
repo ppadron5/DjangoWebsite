d9b063ab193f1352647eda185f6b5e8dbbf799c7
ppadron5 <pedrompadron@gmail.com>
2018-04-26 14:40:27 +0000

    Fixed models.py

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,56 +1,47 @@
 from django.db import models
 
-# Create your models here.
-
 from django.urls import reverse
 
 class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+    
+    def __str__(self):
+        return self.name
 
-	name = models.CharField(max_length=200, help_text="Enter a book genre")
-	
-	def _str_(self):
-		return self.name
 
 class Language(models.Model):
-	name = models.CharField(max_length=200, help_text="Enter a the book's natural language")
-	
-	def _str_(self):
-		return self.name
-
+    name = models.CharField(max_length=200, help_text="Enter a the book's natural language (e.g. English, French, Japanese etc.)")
+    
+    def __str__(self):
+        return self.name
+        
+        
 class Book(models.Model):
-	
-	title = models.CharField(max_length=200)
-	author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-
-	summary = models.TextFields(max_length=1000, help_text="Enter a brief description of the book")
-	isbn = models.CharField('ISBN', max_length=13, help_text="13 character <a href="https://www.sibn-international.org/content/what-isbn">ISBN")
-	genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
-
-	language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
-
-	 
- def display_genre(self):
-
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+      
+    def display_genre(self):
         return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
         display_genre.short_description = 'Genre'
     
     
     def get_absolute_url(self):
-
         return reverse('book-detail', args=[str(self.id)])
 
     def __str__(self):
-
         return self.title
         
         
-import uuid # Required for unique book instances
+import uuid
 from datetime import date
 
-from django.contrib.auth.models import User #Required to assign User as a borrower
+from django.contrib.auth.models import User
 
 class BookInstance(models.Model):
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
     imprint = models.CharField(max_length=200)
@@ -78,13 +69,10 @@ class BookInstance(models.Model):
         permissions = (("can_mark_returned", "Set book as returned"),)   
 
     def __str__(self):
-
-        #return '%s (%s)' % (self.id,self.book.title)
         return '{0} ({1})'.format(self.id,self.book.title)
         
 
 class Author(models.Model):
-
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
@@ -94,10 +82,8 @@ class Author(models.Model):
         ordering = ["last_name","first_name"]
     
     def get_absolute_url(self):
-
         return reverse('author-detail', args=[str(self.id)])
     
 
     def __str__(self):
-
         return '{0}, {1}'.format(self.last_name,self.first_name)
